6cd4aa3d5ac79db452d376837fd52e53b49e9367
ColinJK <colinjk@msn.com>
2025-11-27 15:13:57 +0000

    Adjustment for GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // This ensures the server handles HMR correctly
-    hmr: {
-      overlay: false,
-    },
-  },
+  base: "/sotv/", // <--- ADD THIS LINE (Replace 'shadows-void' with your actual repo name)
 })
